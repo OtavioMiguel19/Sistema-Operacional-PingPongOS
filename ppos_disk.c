7a50f8460630260803784e7eeced2c98ec888d4c
Otavio <otaviomiguel19@gmail.com>
2021-08-28 20:10:16 -0300

    Novo envio

--- ppos_disk.c
+++ ppos_disk.c
@@ -8,3 +8,28 @@
 #if defined(_WIN32) || (!defined(__unix__) && !defined(__unix) && (!defined(__APPLE__) || !defined(__MACH__)))
 #warning Este codigo foi planejado para ambientes UNIX (LInux, *BSD, MacOS). A compilacao e execucao em outros ambientes e responsabilidade do usuario.
 #endif
+
+int disk_mgr_init(int *numBlocks, int *blockSize)
+{
+    // put your customization here
+#ifdef DEBUG
+    printf("\ninit - BEFORE");
+#endif
+    return -1;
+}
+
+int disk_block_read(int block, void *buffer)
+{
+    // put your customization here
+#ifdef DEBUG
+    printf("\ninit - BEFORE");
+#endif
+}
+
+int disk_block_write(int block, void *buffer)
+{
+    // put your customization here
+#ifdef DEBUG
+    printf("\ninit - BEFORE");
+#endif
+}
